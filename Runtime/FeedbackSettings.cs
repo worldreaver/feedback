341446056981e8e46bb0e4545f0a89d6f3a273bf
yenmoc <phongsoyenmoc.diep@gmail.com>
2022-11-25 14:42:47 +0700

    *add api

---
+++ Runtime/FeedbackSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Pancake.Feedback
+{
+    public class FeedbackSettings : ScriptableSettings<FeedbackSettings>
+    {
+        public string token;
+        public bool includeScreenshot = true;
+        public FeedbackBoard board;
+    }
+
+    [Serializable]
+    public class FeedbackBoard
+    {
+        public string id;
+
+        public string[] listNames;
+        public string[] listIds;
+
+        public string[] categoryNames = new string[] {"Feedback", "Bug"};
+        public string[] categoryIds = new string[] {null, null};
+
+        public Label[] labels = new Label[] {new Label("1", null, "Low Priority"), new Label("2", null, "Medium Priority"), new Label("3", null, "High Priority")};
+    }
+}
